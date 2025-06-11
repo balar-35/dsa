0320e5f3fe15c61a0168a38be9bec081404bfabf
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-11 15:29:37 +0530

    completed

---
+++ day_3 task/postassignment.java
@@ -0,0 +1,22 @@
+package day3;
+
+import java.util.Scanner;
+
+public class postassignment {
+	
+	class Node {
+	    int data;
+	    Node next;
+
+	    public Node(int data) {
+	        this.data = data;
+	        this.next = null;
+	    }
+	}
+
+	public class SinglyLinkedList {
+	    Node head = null;
+
+
+	}
+}
